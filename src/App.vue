6cdccee8f18e7ef78a04ce86a249bc36afc3c23f
Anderson <anderson.hung@live.com>
2020-03-25 12:09:04 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <time-series-chart :chart-data="datacollection" :options="options" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimeSeriesChart from "./components/TimeSeriesChart";
+const STORAGE_KEY_DATA = "data";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TimeSeriesChart
+  },
+  data() {
+    return {
+      datacollection: null,
+      options: {
+        responsive: true
+      }
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      let collection = {
+        labels: [],
+        datasets: [
+          {
+            label: "Active",
+            backgroundColor: "#d13e57",
+            data: []
+          }
+        ]
+      };
+      const localStorageData = localStorage.getItem(STORAGE_KEY_DATA);
+
+      if (!localStorageData) {
+        fetch("https://pomber.github.io/covid19/timeseries.json")
+          .then(response => response.json())
+          .then(data => {
+            data["Canada"].forEach(({ date, confirmed, deaths, recovered }) => {
+              collection.labels.push(date);
+              collection.datasets[0][STORAGE_KEY_DATA].push(
+                confirmed - deaths - recovered
+              );
+            });
+          })
+          .finally(() => {
+            this.datacollection = collection;
+            localStorage.setItem(STORAGE_KEY_DATA, JSON.stringify(collection));
+          });
+      } else {
+        this.datacollection = JSON.parse(localStorageData);
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
